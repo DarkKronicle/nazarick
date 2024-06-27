ca1c958c0274330fa3b711fa48c2e2b30617e1cf
DarkKronicle <darkkronicle@gmail.com>
2024-06-27 15:40:05 -0600

    Gen 620: refactor: remove shared values within cocytus/darkkronicle

--- hosts/cocytus/home/darkkronicle.nix
+++ hosts/cocytus/home/darkkronicle.nix
@@ -12,46 +12,4 @@ let
 in
 {
 
-  programs.ssh = {
-    enable = true;
-    matchBlocks = {
-      "gitlab.com" = {
-        host = "gitlab.com";
-        hostname = "gitlab.com";
-        identityFile = [ "/home/darkkronicle/.ssh/id_nazarick" ];
-        extraOptions = {
-          PreferredAuthentications = "publickey";
-        };
-      };
-    };
-  };
-
-  nazarick = {
-    core.nix = enabled;
-
-    cli = {
-      common = {
-        enable = true;
-      };
-      git = {
-        enable = true;
-        userEmail = "darkkronicle@gmail.com";
-        userName = "DarkKronicle";
-      };
-      tui = {
-        zellij = enabled;
-      };
-      nushell = enabled;
-      pager = enabled;
-    };
-
-    tui = {
-      zellij = enabled;
-      neovim = enabled;
-      btop = enabled;
-      yazi = enabled;
-      broot = enabled;
-    };
-
-  };
 }
