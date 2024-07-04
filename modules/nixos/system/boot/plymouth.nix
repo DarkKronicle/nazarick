bb152d43c344c72b3be7afdb749031d839e3e068
DarkKronicle <darkkronicle@gmail.com>
2024-07-04 16:41:56 -0600

    Gen 645: feat: silent startup

--- modules/nixos/system/boot/plymouth.nix
+++ modules/nixos/system/boot/plymouth.nix
@@ -19,14 +19,17 @@ in
 
     boot = {
       initrd.systemd.enable = true;
-      # consoleLogLevel = 0;
-      # initrd.verbose = false;
-      # kernelParams = [
-      # "quiet"
-      # "boot.shell_on_fail"
-      # "loglevel=3"
-      # "rd.systemd.show_status=false"
-      # ];
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
 
       plymouth = {
         enable = true;
