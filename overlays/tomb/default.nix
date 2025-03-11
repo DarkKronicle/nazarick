584bbff2439f122108dee8b12e3850d5b61583b6
DarkKronicle <darkkronicle@gmail.com>
2025-03-10 23:35:07 -0600

    Gen 1074: feat: tomb kdf extras

---
+++ overlays/tomb/default.nix
@@ -0,0 +1,33 @@
+{ pkgs-unstable, ... }:
+
+final: prev:
+
+{
+  tomb =
+    let
+      extras-kdf = pkgs-unstable.stdenv.mkDerivation {
+        pname = "tomb-extras-kdf-keys";
+        version = pkgs-unstable.tomb.version;
+
+        src = "${pkgs-unstable.tomb.src}/extras/kdf-keys";
+
+        nativeBuildInputs = with pkgs-unstable; [
+          libgcrypt
+        ];
+
+        installPhase = ''
+          install -Dm755 tomb-kdb-pbkdf2 $out/bin/tomb-kdb-pbkdf2
+          install -Dm755 tomb-kdb-pbkdf2-getiter $out/bin/tomb-kdb-pbkdf2-getiter
+          install -Dm755 tomb-kdb-pbkdf2-gensalt $out/bin/tomb-kdb-pbkdf2-gensalt
+        '';
+      };
+    in
+    pkgs-unstable.symlinkJoin {
+      inherit (pkgs-unstable.tomb) name version;
+      paths = [ pkgs-unstable.tomb ];
+      buildInputs = [ pkgs-unstable.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/tomb --prefix PATH : ${pkgs-unstable.lib.makeBinPath [ extras-kdf ]}
+      '';
+    };
+}
