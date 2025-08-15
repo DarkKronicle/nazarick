76a67b51424bc11883a2205a9c23d9f90e195dc6
DarkKronicle <darkkronicle@gmail.com>
2025-08-15 09:34:01 -0600

    Gen 1202: fix: properly configure process-compose theme

--- modules/home/gui/swww.nix
+++ modules/home/gui/swww.nix
@@ -66,7 +66,7 @@ in
 
       Service = {
         Type = "exec";
-        ExecStart = "${pkgs.swww}/bin/swww-daemon";
+        ExecStart = "${pkgs.swww}/bin/swww-daemon --no-cache";
       };
     };
 
